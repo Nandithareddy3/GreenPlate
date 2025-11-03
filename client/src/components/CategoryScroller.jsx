6ca5a606aca1db7d53c156572b471f5284799616
Nandithareddy3 <nanditha.daram3@gmail.com>
2025-11-03 21:15:25 +0530

    Update CategoryScroller.jsx

--- client/src/components/CategoryScroller.jsx
+++ client/src/components/CategoryScroller.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './CategoryScroller.module.css';
 
-// These categories must match your backend 'listingModel.js' enum
 const categories = [
   'All',
   'Bakery & Breads',
